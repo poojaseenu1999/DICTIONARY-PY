89bce60e530e8ad0313e0c5db6176fc004be44d9
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-19 01:42:44 +0530

    adding conditionals

--- app1.py
+++ app1.py
@@ -8,7 +8,14 @@ def translate(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w,data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+        yn =input("Did you mean %s instead?enter y if yes or n if no:"   % get_close_matches(w, data.keys())[0])
+        if yn == 'y':
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == 'n':
+            return "The word doesnt exist. please check it."
+        else:
+             return "sorry! we can't understand."
+
     else:
         return "this word doesnt exist!"
 
